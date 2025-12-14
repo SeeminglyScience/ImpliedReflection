07b8bcec945da7b519bb9652561d0a54ad088cfd
Patrick Meinecke <seeminglyscience@gmail.com>
2025-12-14 16:36:26 -0500

    Update harmony to support up to net10

--- build.ps1
+++ build.ps1
@@ -1,13 +1,25 @@
 [CmdletBinding()]
 param(
-    [switch] $Force
+    [ValidateSet('Debug', 'Release')]
+    [string] $Configuration = 'Release',
+
+    [Parameter()]
+    [switch] $Force,
+
+    [Parameter()]
+    [switch] $Publish
 )
 end {
-    & "$PSScriptRoot\tools\AssertRequiredModule.ps1" InvokeBuild 5.8.4 -Force
+    & "$PSScriptRoot\tools\AssertRequiredModule.ps1" InvokeBuild 5.12.2 -Force
     $invokeBuildSplat = @{
         Task = 'Build'
         File = "$PSScriptRoot/ImpliedReflection.build.ps1"
-        Configuration = 'Release'
+        Force = $Force.IsPresent
+        Configuration = $Configuration
+    }
+
+    if ($Publish) {
+        $invokeBuildSplat['Task'] = 'Publish'
     }
 
     Invoke-Build @invokeBuildSplat
